1c244c3bf8e5cf7f1ab90242729c272a9a4c58bc
akshaysalokhe7 <akshaysalokhe@hotmail.com>
2024-07-22 15:01:36 +0530

    added api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppService } from './app.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 export interface TicketData {
   id: number;
@@ -22,15 +25,15 @@ export interface TicketData {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  imports: [RouterOutlet, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule, MatSelectModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tickets';
   STATUSES: string[] = ["To Do", "In Progress", "Done"];
-  displayedColumns: string[] = ['id', 'title', 'desc', 'status'];
-  dataSource: MatTableDataSource<TicketData>;
+  displayedColumns: string[] = ['id', 'title', 'description', 'status', 'delete'];
+  dataSource: MatTableDataSource<TicketData> = new MatTableDataSource();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -38,14 +41,21 @@ export class AppComponent {
   ticketAddForm: FormGroup;
     
 
-  constructor() {
+  constructor(private service: AppService) {
     this.ticketAddForm = new FormGroup({
       title: new FormControl('', [Validators.required,Validators.minLength(4)]),
-      desc: new FormControl(''),
+      description: new FormControl(''),
       status: new FormControl('To Do',[Validators.required])
     });
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource([{id:1, title:"test", desc:"desc", status:"In Progress"}]);
+    this.getTickets();
+  }
+
+  getTickets(){
+    this.service.getTickets().subscribe((res:any)=>{
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
 
   ngAfterViewInit() {
@@ -64,6 +74,17 @@ export class AppComponent {
   }
 
   onSubmit(){
-    console.log(this.ticketAddForm);
+    this.service.addTicket(this.ticketAddForm.value).subscribe(res=>{
+      this.getTickets();
+      this.ticketAddForm.reset();
+    });
+  }
+
+  onDelete(id:number){
+    if(confirm('Are you sure?')){
+      this.service.deleteTicket(id).subscribe(res=>{
+        this.getTickets();
+      });
+    }
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
+import { TicketData } from './app.component';
+import { HttpClient } from '@angular/common/http'
 
-const serveURL = "http://localhost:3000";
+const serveURL = "http://localhost:3000/tickets";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  addTicket(data:TicketData){
+    return this.http.post(`${serveURL}/add`,data);
+  }
 
+  getTickets(){
+    return this.http.get(`${serveURL}/getAll`);
+  }
 
+  deleteTicket(id:number){
+    return this.http.delete(`${serveURL}/delete/${id}`);
+  }
 }
